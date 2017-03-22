a8a38ac2f2dbe5fd7ae1185753bc3c6803d227c3
lasiaf <muhammadfaisal.uns@gmail.com>
2017-03-22 13:30:54 +0700

    Masukan file hitung.java

---
+++ Hitung.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//import java.util.Scanner;
+
+package hitung;
+
+/**
+ *
+ * @author Handa Kusuo
+ */
+import java.util.Scanner;
+public class Hitung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner masukan = new Scanner (System.in);
+        int a, b;
+        boolean c = true;
+        
+        if (c = false){
+            System.out.printf("Masukan : ");
+            a = masukan.nextInt();
+            b = (a * 4 * 3)/2;
+            System.out.printf("Output : "+b);
+        }
+        else{
+            System.out.printf("Tidak memenuhi syarat %.2f\n");
+        }
+    }
+    
+}
